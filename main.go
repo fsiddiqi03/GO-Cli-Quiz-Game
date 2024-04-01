ea9b166226e34526a111f107e4d5e632c1f9a7f6
fsiddiqi03 <farissiddiqi@gmail.com>
2024-04-01 01:18:43 -0500

    added file error handling

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of question, answer")
 	flag.Parse()
-	_ = csvFilename
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Printf("Failed to open the CSV file: %s\n", *csvFilename)
+		os.Exit(1)
+	}
+
+	_ = file
+
 }
