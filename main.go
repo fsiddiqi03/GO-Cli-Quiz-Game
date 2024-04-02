e23b468eae498f7c0879279df53635aecc34344e
fsiddiqi03 <farissiddiqi@gmail.com>
2024-04-02 15:04:21 -0500

    create parseLines function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
@@ -12,10 +13,38 @@ func main() {
 
 	file, err := os.Open(*csvFilename)
 	if err != nil {
-		fmt.Printf("Failed to open the CSV file: %s\n", *csvFilename)
+		// sends message as a string
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
 		os.Exit(1)
 	}
 
-	_ = file
+	r := csv.NewReader(file)
+	line, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provide csv file.")
+	}
+	fmt.Print(line)
+}
 
+func parseLines(lines [][]string) []problem {
+	// create a slice with the problem struct, set len to len of slices
+	ret := make([]problem, len(lines))
+	for i, line := range lines { // loop through the 2D slices and creates a problem, while adding them to res
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
+}
+
+// create a problem object
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Print(msg)
+	os.Exit(1)
 }
