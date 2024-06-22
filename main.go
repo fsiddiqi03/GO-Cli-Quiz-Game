e94b29c00b53464a95ccebac0feaa0b2c83d651a
fsiddiqi03 <farissiddiqi@gmail.com>
2024-06-21 21:35:02 -0500

    added function that displays questions

--- main.go
+++ main.go
@@ -5,50 +5,56 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of question, answer")
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of question,answer")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
 	if err != nil {
-		// sends message as a string
-		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
-		os.Exit(1)
+		exit(fmt.Sprintf("Failed to open the CSV file: %s with error %s\n", *csvFilename, err))
 	}
 
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
-		exit("Failed to parse the provide csv file.")
+		exit("Failed to parse the provided csv file.")
 	}
 	problems := parseLines(lines)
-	
-	for i, p := problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1 p.q)
+	correct := 0
+
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		answer = strings.TrimSpace(answer)
+		if answer == p.a {
+			correct++
+		}
 	}
+
+	fmt.Printf("You got %d out of %d correct.\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
-	// create a slice with the problem struct, set len to len of slices
 	ret := make([]problem, len(lines))
-	for i, line := range lines { // loop through the 2D slices and creates a problem, while adding them to res
+	for i, line := range lines {
 		ret[i] = problem{
 			q: line[0],
-			a: line[1],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
 }
 
-// create a problem object
 type problem struct {
 	q string
 	a string
 }
 
 func exit(msg string) {
-	fmt.Print(msg)
+	fmt.Println(msg)
 	os.Exit(1)
 }
